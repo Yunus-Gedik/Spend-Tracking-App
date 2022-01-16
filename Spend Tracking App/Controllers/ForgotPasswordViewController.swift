ad8e7af3ec0b62dbb3a24eecbf4c79bdd24d84a1
Yunus-Gedik <yunus-gedik@outlook.com>
2022-01-16 22:55:46 +0300

    feat: reset password now works

--- Spend Tracking App/Controllers/ForgotPasswordViewController.swift
+++ Spend Tracking App/Controllers/ForgotPasswordViewController.swift
@@ -6,16 +6,26 @@
 //
 
 import UIKit
+import Firebase
 
 class ForgotPasswordViewController: UIViewController {
-
+    @IBOutlet var emailTextBox: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func send(_ sender: UIButton) {
+        if let email = emailTextBox.text{
+            Auth.auth().sendPasswordReset(withEmail: email) { error in
+                print(" ")
+            }
+            self.navigationController?.popViewController(animated: true)
+        }
+    }
+    
     /*
     // MARK: - Navigation
 
